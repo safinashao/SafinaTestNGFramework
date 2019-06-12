53a150eae84e482b72b245b4e83ba2af2952e9d9
safinashao <talyshao@gmail.com>
2019-06-12 14:58:47 -0500

    adding new test script2

---
+++ crm-testng/src/main/java/page/GetFromMaster2.java
@@ -0,0 +1,14 @@
+package page;
+
+public class GetFromMaster2 {
+
+	public void test() {
+		int a = 10;
+		int b = 30;
+		int c = a + b;
+		System.out.println(c);
+		for (int i = 0; i < 9; i++) {
+			c = i;
+		}
+	}
+}
