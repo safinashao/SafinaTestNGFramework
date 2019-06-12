2bbbefb2e47ada3d57b06c01f2ebb05813a4a2c6
safinashao <talyshao@gmail.com>
2019-06-12 17:02:52 -0500

    adding two new changes

--- crm-testng/src/main/java/page/GetFromMaster.java
+++ crm-testng/src/main/java/page/GetFromMaster.java
@@ -5,9 +5,11 @@ public class GetFromMaster {
 	public void test() {
 		int a = 10;
 		int b = 30;
-		int b30 = 30;
-		int b40 = 30;
-		int b50 = 30;
+		int b15 = 40;
+		int b30 = 50;
+		int b45 = 60;
+		int b60 = 70;
+		int b75 = 80;
 		int c = a + b;
 		System.out.println(c);
 		for (int i = 0; i < 9; i++) {
