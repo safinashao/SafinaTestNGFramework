bb9a79ec3d1c2cb8eae7d120a0dccf7ffb64c5c7
safinashao <talyshao@gmail.com>
2019-06-13 11:15:35 -0500

    adding new changes in the file

--- crm-testng/src/main/java/page/GetFromMaster.java
+++ crm-testng/src/main/java/page/GetFromMaster.java
@@ -9,7 +9,7 @@ public class GetFromMaster {
 		int b30 = 50;
 		int b45 = 60;
 		int b60 = 70;
-		int b75 = 80;
+		int c1 = 100;
 		int c = a + b;
 		System.out.println(c);
 		for (int i = 0; i < 9; i++) {
